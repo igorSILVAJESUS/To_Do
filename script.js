e5daa01eb8a91e35d74446a63f7e09ef1adb7fcd
igor jesus silva <igorsilva@colegiopaulodetarso.com.br>
2026-03-17 14:48:17 +0000

    Estrtura HTML

---
+++ script.js
@@ -0,0 +1,7 @@
+const taskBtn = document.querySelector("#taskBtn")
+const taskList = document.querySelector("#taskList")
+const taskInput= document.querySelector("#taskImput")
+
+taskBtn.addEventListener("click", function(){
+    alert(" cliquei !!!")
+})
