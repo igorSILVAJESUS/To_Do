12fc19fbc0b9c64e4137770f578e5e66d3f43070
igor jesus silva <igorsilva@colegiopaulodetarso.com.br>
2026-03-31 14:32:30 +0000

    kkkkkkk

--- script.js
+++ script.js
@@ -2,19 +2,35 @@ const taskBtn = document.querySelector("#taskBtn")
 const taskList = document.querySelector("#taskList")
 const taskInput= document.querySelector("#taskInput")
 
-taskBtn.addEventListener("click", function(){
+
+function createSpan(taskInputValue){
+    const span = document.createElement('span');
+    span.textContent = taskInputValue;
+    return span;
+}
+
+function createDeletebutton(li){
+    const button = document.createElement('button');
+    button.textContent = 'Excluir'
+    button.addEventListener('click', function(){
+        li.remove(); 
+    })
+    return button;
+}
+
+
+function addTask(){
      const li = document.createElement('li');
      const taskInputValue = taskInput.value;
-     const span = document.createElement('span');
-     span.textContent = taskInputValue;
-     const button = document.createElement('button');
-     button.textContent = 'Excluir'
-      button.addEventListener('click', function(){
-        li.remove();
-    })
-
+     const span = createSpan(taskInputValue);
+     const button = createDeletebutton(li);
+     if (taskInputValue == ''){
+        return
+     }
     li.appendChild(span)
     li.appendChild(button)
 
     taskList.appendChild(li);
-})
+}    
+
+taskBtn.addEventListener("click",addTask)
